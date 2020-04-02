e5afbe75e9d663169d9e53e6395fc89872292cf1
neverCase <1024769485@qq.com>
2020-04-02 15:20:13 +0800

    redis-slave: run.sh adds a series of `sed *` commands

--- examples/redis-slave/run.sh
+++ examples/redis-slave/run.sh
@@ -1,35 +1,26 @@
 #!/usr/bin/env bash
 
-defaultConf="redis.conf"
-file="default.conf"
-if [[ "$ENV_REDIS_CONF" ]]
+defaultConf="/redis.conf"
+if [[ -z "$ENV_REDIS_CONF" ]]
 then
-	file="$ENV_REDIS_CONF"
-else
-    echo "error: no ENV_REDIS_CONF"
+    echo "error: no env ENV_REDIS_CONF"
     exit
 fi
 
 cp ${defaultConf} ${ENV_REDIS_CONF}
-rm ${file}
-touch ${file}
 
-echo "save 900 1" >> ${file}
-echo "save 300 10" >> ${file}
-echo "save 60 10000" >> ${file}
-
-if [[ "$ENV_REDIS_MASTER" ]] & [[ "$ENV_REDIS_MASTER_PORT" ]]; then
-	echo "slaveof $ENV_REDIS_MASTER $ENV_REDIS_MASTER_PORT" >> ${file}
+if [[ "$ENV_REDIS_MASTER" ]] && [[ "$ENV_REDIS_MASTER_PORT" ]]; then
+	sed -i "s/# replicaof <masterip> <masterport>/replicaof ${ENV_REDIS_MASTER} ${ENV_REDIS_MASTER_PORT}/g"  ${ENV_REDIS_CONF}
 fi
 
 if [[ "$ENV_REDIS_DIR" ]]
 then
-	echo "dir $ENV_REDIS_DIR" >> ${file}
+	sed -i "s#dir ./#dir ${ENV_REDIS_DIR}#g"  ${ENV_REDIS_CONF}
 fi
 
 if [[ "$ENV_REDIS_DBFILENAME" ]]
 then
-	echo "dbfilename $ENV_REDIS_DBFILENAME" >> ${file}
+	sed -i "s/dbfilename dump.rdb/dbfilename ${ENV_REDIS_DBFILENAME}/g" ${ENV_REDIS_CONF}
 fi
 
 shutdownSave() {
@@ -38,6 +29,6 @@ shutdownSave() {
 
 trap "echo 'get the signal,redis-server would shut down and save before release container'; shutdownSave" SIGHUP SIGINT SIGQUIT SIGTERM
 
-redis-server ${file} &
+redis-server ${ENV_REDIS_CONF} &
 
 wait
