c0d331c220eb7911a653a82614dd929331125d57
neverCase <1024769485@qq.com>
2020-04-02 12:03:07 +0800

    redis-slave: add default conf and modify run.sh

--- examples/redis-slave/run.sh
+++ examples/redis-slave/run.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
+defaultConf="redis.conf"
 file="default.conf"
-cat ${file}
+if [[ "$ENV_REDIS_CONF" ]]
+then
+	file="$ENV_REDIS_CONF"
+else
+    echo "error: no ENV_REDIS_CONF"
+    exit
+fi
+
+cp ${defaultConf} ${ENV_REDIS_CONF}
 rm ${file}
 touch ${file}
+
+echo "save 900 1" >> ${file}
+echo "save 300 10" >> ${file}
+echo "save 60 10000" >> ${file}
 
 if [[ "$ENV_REDIS_MASTER" ]] & [[ "$ENV_REDIS_MASTER_PORT" ]]; then
 	echo "slaveof $ENV_REDIS_MASTER $ENV_REDIS_MASTER_PORT" >> ${file}
@@ -19,4 +32,12 @@ then
 	echo "dbfilename $ENV_REDIS_DBFILENAME" >> ${file}
 fi
 
-redis-server ${file}
+shutdownSave() {
+   redis-cli shutdown save
+}
+
+trap "echo 'get the signal,redis-server would shut down and save before release container'; shutdownSave" SIGHUP SIGINT SIGQUIT SIGTERM
+
+redis-server ${file} &
+
+wait
